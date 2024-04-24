fa43c46152e8cc38cbc9876d96b657ecd3cea745
Vo Minh Vo <vominhvo@gmail.com>
2024-04-25 05:05:22 +0700

    Update pipeline

--- src/main/java/info/cloudmastery/microservices/notification/controller/EmailController.java
+++ src/main/java/info/cloudmastery/microservices/notification/controller/EmailController.java
@@ -2,6 +2,7 @@ package info.cloudmastery.microservices.notification.controller;
 
 import info.cloudmastery.microservices.notification.dto.ResponseData;
 import info.cloudmastery.microservices.notification.dto.response.EmailResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,10 @@ public class EmailController {
     @PostMapping
     public ResponseData<EmailResponse> sendEmail() {
         return ResponseData.of(new EmailResponse());
+    }
+
+    @GetMapping
+    public ResponseData<String> ping() {
+        return ResponseData.of("OK!");
     }
 }
